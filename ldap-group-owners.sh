91be7a51a50a2933fb9df60face6bcd07a8366b5
Colin Nolan <colin-nolan@users.noreply.github.com>
2018-03-15 15:53:07 +0000

    Import from Gist.

---
+++ ldap-group-owners.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+groups=$@
+
+module purge
+module add hgi/ldapvi/latest
+
+if [[ ! -r ~/.ldapvirc ]]; then
+    >&2 echo "~/.ldapvirc not readable" 
+    exit 1
+fi
+
+declare -a parsedGroups
+for group in "$@"; do
+    parsedGroups[${#parsedGroups[@]}]=$(
+        echo -n "\"${group}\":"
+        echo -n $(ldapvi --out "(&(cn=${group})(objectClass=posixGroup))" | grep -e ^owner | grep -Po '(?<=uid=)([^,]*)' | jq -R . | jq -s '.')
+        echo -n ""
+    )
+done
+echo "{$(IFS=, ; echo "${parsedGroups[*]}")}" | jq "."
