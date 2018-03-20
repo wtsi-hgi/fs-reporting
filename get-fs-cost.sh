0b21e5e0006175702f02c89ccf0d509a44e034ca
Christopher Harrison <ch12@sanger.ac.uk>
2018-03-20 12:40:14 +0000

    Prettier

--- get-fs-cost.sh
+++ get-fs-cost.sh
@@ -1,33 +1,22 @@
 # Get filesystem cost per terabyte year
 # Christopher Harrison <ch12@sanger.ac.uk>
 
-# n.b., This doesn't need to be any prettier :P
+declare -A _FS_COSTS=(
+  [lustre]="150"
+
+  # TODO Find costs
+  # [nfs]=...
+  # [warehouse]=...
+  # [irods]=...
+)
 
 get_fs_cost() {
   local fs_type="$1"
 
-  case "${fs_type}" in
-    "lustre")
-      echo "150"
-      ;;
+  if ! [[ ${_FS_COSTS["${fs_type}"]+_} ]]; then
+    >&2 echo "No costing data for \"${fs_type}\" filesystems"
+    exit 1
+  fi
 
-    "nfs")
-      # TODO Find cost...
-      echo "??"
-      ;;
-
-    "warehouse")
-      # TODO Find cost...
-      echo "??"
-      ;;
-
-    "irods")
-      # TODO Find cost...
-      echo "??"
-      ;;
-
-    *)
-      exit 1
-      ;;
-  esac
+  echo "${_FS_COSTS["${fs_type}"]}"
 }
